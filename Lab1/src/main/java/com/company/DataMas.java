445f3122ce7c17a8521d128ffb55867e3f62fb6f
alex2704 <34662549+alex2704@users.noreply.github.com>
2019-10-30 16:22:30 +0300

    Сделал сортировку по фамилии и добавил поиск по id и ФИ;

--- Lab1/src/main/java/com/company/DataMas.java
+++ Lab1/src/main/java/com/company/DataMas.java
@@ -1,6 +1,10 @@
 package com.company;
 
+import org.apache.commons.lang.ObjectUtils;
 import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Массив который хранит объекты экземпляра Person.
@@ -120,5 +124,57 @@ public class DataMas {
             }
             arr[j+1] = current;
         }
+    }
+
+    /**
+     * Быстрая сортировка по фамилии по возрастанию
+     */
+    public void sortBySurname(){
+        for (int i=0; i<arr.length; i++){
+            Person min = arr[i];
+            int minId = i;
+            for (int j=i+1; j<arr.length; j++){
+                int m  = 0;
+                while (arr[j].getSurname().substring(m, m + 1).equals(min.getSurname().substring(m, m + 1)) &&
+                        m + 1 < arr[j].getSurname().length() && m + 1 < min.getSurname().length())
+                    m++;
+                if(arr[j].getSurname().charAt(m) < min.getSurname().charAt(m)){
+                    min = arr[j];
+                    minId = j;
+                }
+                m = 0;
+            }
+            Person temp = arr[i];
+            arr[i] = min;
+            arr[minId] = temp;
+        }
+    }
+
+    /**
+     * Поиск по id
+     * @param id идентификатор
+     * @return объект класса Person
+     */
+    public Person search(int id){
+        for (Person person : arr) {
+            if (person.getId() == id)
+                return person;
+        }
+        return null;
+    }
+
+    /**
+     * Поиск по имени и фамилии
+     * @param Name имя
+     * @param Surname фамилия
+     * @return список объектов класса Person с данным именем и фамилией
+     */
+    public ArrayList<Person> search(String Name, String Surname){
+        ArrayList<Person> result = new ArrayList<Person>();
+        for (Person person : arr) {
+            if (person.getSurname().equals(Surname) && person.getName().equals(Name))
+                result.add(person);
+        }
+        return result;
     }
 }
--- Lab1/src/main/java/com/company/Main.java
+++ Lab1/src/main/java/com/company/Main.java
@@ -2,15 +2,17 @@ package com.company;
 
 import org.joda.time.LocalDate;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         DataMas arr = new DataMas();
-        Person person = new Person(5, "иван", "Пупкин", "Васильевич", "м",
+        Person person = new Person(5, "иван", "Аыупкин", "Васильевич", "м",
                 new LocalDate(2004, 4, 27));
-        Person person2 = new Person(3, "Саша", "Пупкин", "Васильевич", "м",
+        Person person2 = new Person(3, "Саша", "Аупкина", "Васильевич", "м",
                 new LocalDate(1999, 4, 27));
-        Person person3 = new Person(2, "Вася", "Пупкин", "Васильевич", "м",
+        Person person3 = new Person(2, "Вася", "Вупкин", "Васильевич", "м",
                 new LocalDate(2007, 4, 27));
         arr.add(person);
         arr.add(person);
@@ -19,10 +21,12 @@ public class Main {
         arr.add(person3);
         arr.add(person2);
 
-        System.out.println();
+        arr.sortBySurname();
+        ArrayList<Person> result = arr.search(5);
+        System.out.println(result.get(0).getSurname());
+        System.out.println("Завершен");
         for (int i=0; i<arr.getLength(); i++){
-            arr.sortByBirthday();
-            System.out.println(arr.get(i).getAge());
+            System.out.println(arr.get(i).getSurname());
         }
     }
 }
