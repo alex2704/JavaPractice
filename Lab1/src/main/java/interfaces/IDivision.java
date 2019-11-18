74df2800f4d02d3880456d0991803bbd9cebdd94
alex2704 <34662549+alex2704@users.noreply.github.com>
2019-11-18 13:56:59 +0300

    Добавил интерфейсы;

---
+++ Lab1/src/main/java/interfaces/IDivision.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+public interface IDivision {
+    public Integer getId();
+
+    public void setId(Integer id);
+
+    public String getName();
+
+    public void setName(String name);
+}
