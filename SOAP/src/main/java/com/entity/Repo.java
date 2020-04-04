c522b4fb330bce35f538ce9b8f29a30949c48eef
alex2704 <34662549+alex2704@users.noreply.github.com>
2020-04-05 00:26:34 +0300

    Добавил entities;

---
+++ SOAP/src/main/java/com/entity/Repo.java
@@ -0,0 +1,15 @@
+package com.entity;
+
+import java.util.List;
+
+public class Repo {
+    private List<Person> persons;
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+}
